0db5c50dee70f7263b168393aaae97486b489a3d
Ericsson22 <33991132+Ericsson22@users.noreply.github.com>
2018-07-29 21:01:43 +0200

    Merge branch 'master' of https://github.com/Ericsson22/NotifyMe into 29-07-20-27

---
+++ app/src/main/java/com/example/manue/notifyme/DaoAccess.java
@@ -0,0 +1,10 @@
+package com.example.manue.notifyme;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+
+@Dao
+public interface DaoAccess {
+    @Insert
+    void insertTask(Task task);
+}
---
+++ app/src/main/java/com/example/manue/notifyme/Task.java
@@ -0,0 +1,49 @@
+package com.example.manue.notifyme;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity
+public class Task {
+    @NonNull
+    @PrimaryKey (autoGenerate = true)
+    //taskId created automatically (hopefully)
+    private int taskId;
+
+    private String taskTitle;
+    private String taskDescription;
+    //spinner gives back an id; e.g. 1 for "1 hour before", 2 for "24 hours before" etc.
+    private int reminderId;
+    //differentiates between solved and unsolved; tasks are shown in different activities
+    private boolean solved;
+
+    public void setTaskId(@NonNull int taskId) {
+        this.taskId = taskId;
+    }
+
+    public void setTaskTitle(@NonNull String taskTitle){
+        this.taskTitle = taskTitle;
+    }
+
+    public void setTaskDescription(String taskDescription){
+        this.taskDescription = taskDescription;
+    }
+
+    public void setReminderId(@NonNull int reminderId){
+        this.reminderId = reminderId;
+    }
+
+    public boolean setTaskState(@NonNull boolean solved){
+        if(solved){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public Task(){
+
+    }
+
+}
