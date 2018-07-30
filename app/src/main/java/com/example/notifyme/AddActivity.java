2ea991ce2d3d922f5387e61a6e10c557fb31cf30
Ericsson22 <33991132+Ericsson22@users.noreply.github.com>
2018-07-30 18:22:46 +0200

    AddActivity angepasst, mit Sachen die noch getan werden müssen

--- app/src/main/java/com/example/notifyme/AddActivity.java
+++ app/src/main/java/com/example/notifyme/AddActivity.java
@@ -8,14 +8,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import java.util.Date;
 
 
 public class AddActivity extends AppCompatActivity{
 
     private Spinner spinnerRemeberTime;
     private Button addButton;
+    private EditText title;
+    private EditText description;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +48,11 @@ public class AddActivity extends AppCompatActivity{
         spinnerRemeberTime = findViewById(R.id.spinner_remember);
         initSpinner(spinnerRemeberTime,R.array.remember_array);
 
+        title = findViewById(R.id.input_title);
+        description = findViewById(R.id.input_description);
+
         addButton = findViewById(R.id.button_to_add);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(getApplicationContext(),
-                "Durch diesen Klick sollten die Werte als neues Task Objekt gespeichert und in die Datenbank übertragen werden",
-                        Toast.LENGTH_SHORT);
-                toast.show();
-            }
-        });
+        initButton();
     }
 
     private void initSpinner(Spinner spinner, int arrayID) {
@@ -83,7 +84,30 @@ public class AddActivity extends AppCompatActivity{
             }
         });
     }
+        private void initButton(){
+            addButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Durch diesen Klick sollten die Werte als neues Task Objekt gespeichert und in die Datenbank übertragen werden",
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+                   String titleInputString = title.getText().toString();
+                   String descriptionInputString = description.getText().toString();
 
+                    //int reminderId = aus Spinner
+                    //Date dateTaskFinished = Date aus Spinner, vllt noch in ne bestimmte Form gebracht..
+                    //int priority = aus Spinner? oder neue Auswahlmöglichkeit
+                    boolean solved = false;
+                    //wird wohl keiner auf die Idee kommen, sich ne neue Erinnerung für was bereits abgeschlossenes zu machen, oder?
+                    //falls doch, sollten wir das vielleicht als Fehler hier abfangen...
+
+                    //Task task = new Task(id, titleInputString, descriptionInputString, reminderId, dateTaskFinished, priority, solved);
+                            //id muss immer weiter hoch zählen, darf nie 2 mal den selben wert geben, also höchsten wert irgendwo abspeichern -> ++
+                            //
+                }
+            });
+        }
 
 }
 
