68aa6c5deb96de9989915101c916a08797702990
Ericsson22 <33991132+Ericsson22@users.noreply.github.com>
2018-07-30 15:31:55 +0200

    Task updated
    Konstruktor, Getter

--- app/src/main/java/com/example/notifyme/Task.java
+++ app/src/main/java/com/example/notifyme/Task.java
@@ -4,19 +4,39 @@ import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
 
+import java.util.Date;
+
 @Entity
 public class Task {
     @NonNull
     @PrimaryKey (autoGenerate = true)
-    //taskId created automatically (hopefully)
+    //taskId created automatically and ongoing(no number twice)
     private int taskId;
-
     private String taskTitle;
     private String taskDescription;
     //spinner gives back an id; e.g. 1 for "1 hour before", 2 for "24 hours before" etc.
     private int reminderId;
+
+    //Date and Time when Task is finished and put into solved Tasks
+    private Date taskFinished;
+
+    //priority of this Task
+    private int priority;
     //differentiates between solved and unsolved; tasks are shown in different activities
     private boolean solved;
+
+    public Task(int taskId, String taskTitle, String taskDescription, int reminderId, Date taskFinished, int priority, boolean solved){
+        this.taskId = taskId;
+        this.taskTitle = taskTitle;
+        this.taskDescription = taskDescription;
+        this. reminderId = reminderId;
+        this.taskFinished = taskFinished;
+        this.priority = priority;
+        this.solved = solved;
+
+        //addEntryInDatabase(this);
+    }
+
 
     public void setTaskId(@NonNull int taskId) {
         this.taskId = taskId;
@@ -41,9 +61,28 @@ public class Task {
             return false;
         }
     }
-    
-    public Task(){
 
+    public Date getTaskFinished() {
+        return taskFinished;
     }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setTaskFinished(Date taskFinished) {
+        this.taskFinished = taskFinished;
+    }
+
+    public void setSolved(boolean solved) {
+        this.solved = solved;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+
+
 
 }
