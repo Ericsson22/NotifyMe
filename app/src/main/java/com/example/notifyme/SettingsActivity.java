7e2db611e43773cf9ea8d548b329b8d755f65842
4manu20 <36849637+4manu20@users.noreply.github.com>
2018-08-05 13:58:46 +0200

    Titel wird jetzt in Action angezeigt // Preference rumgeficke

--- app/src/main/java/com/example/notifyme/SettingsActivity.java
+++ app/src/main/java/com/example/notifyme/SettingsActivity.java
@@ -1,17 +1,36 @@
 package com.example.notifyme;
 
+import android.os.Build;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.Toolbar;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.prefs.PreferenceChangeEvent;
+import java.util.prefs.PreferenceChangeListener;
 
+public class SettingsActivity extends PreferenceActivity implements PreferenceChangeListener  {
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //Activity_Preference kann später gelöscht und durch preference.xml ersetz werden
+        //setContentView(R.layout.activity_preference);
+        addPreferencesFromResource(R.xml.preference);
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -24,4 +43,8 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void preferenceChange(PreferenceChangeEvent evt) {
+
+    }
 }
